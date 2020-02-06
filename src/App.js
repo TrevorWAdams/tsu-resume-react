9e1a4786aadac226148cccb8fc9b469005828094
Trevor Adams <trevorwadams@gmail.com>
2020-02-05 20:18:53 -0700

    task: add stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import mandalorian from "./mandalorian_pic.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="twelve column">
+          <img className="bio-image" src={mandalorian} alt="bio" />
+
+          <h2>Britney Adams</h2>
+          <h5>Supportability Specialist</h5>
+
+          <span>
+            <strong>Email:</strong>{" "}
+            <a href="adams.britneyr@gmail.com">adams.britneyr@gmail.com</a>
+          </span>
+          <br />
+          <span>
+            <strong>Phone:</strong> 208-949-9859
+          </span>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="row">
+        <div className="twelve column">
+          <h4>Work History</h4>
+
+          <h6>
+            <strong>Pied Piper</strong> 2017-Present
+          </h6>
+          <p>
+            Beard twee live-edge, pop-up kogi pabst etsy shoreditch small batch
+            vegan. Skateboard brooklyn gochujang 90's ennui pour-over before
+            they sold out swag cliche wolf butcher cronut sriracha shoreditch
+            bushwick. Activated charcoal jean shorts chillwave four dollar toast
+            meggings health goth cornhole.
+          </p>
+
+          <h6>
+            <strong>Hooli</strong> 2014-2017
+          </h6>
+          <p>
+            Beard twee live-edge, pop-up kogi pabst etsy shoreditch small batch
+            vegan. Skateboard brooklyn gochujang 90's ennui pour-over before
+            they sold out swag cliche wolf butcher cronut sriracha shoreditch
+            bushwick. Activated charcoal jean shorts chillwave four dollar toast
+            meggings health goth cornhole.
+          </p>
+
+          <h6>
+            <strong>Dunder Mifflin</strong> 2010-2014
+          </h6>
+          <p>
+            Beard twee live-edge, pop-up kogi pabst etsy shoreditch small batch
+            vegan. Skateboard brooklyn gochujang 90's ennui pour-over before
+            they sold out swag cliche wolf butcher cronut sriracha shoreditch
+            bushwick. Activated charcoal jean shorts chillwave four dollar toast
+            meggings health goth cornhole.
+          </p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="twelve column">
+          <h4>Education</h4>
+
+          <h6>
+            <strong>Super Hacker U</strong> 2010
+          </h6>
+          <p>
+            Beard twee live-edge, pop-up kogi pabst etsy shoreditch small batch
+            vegan. Skateboard brooklyn gochujang 90's ennui pour-over before
+            they sold out swag cliche wolf butcher cronut sriracha shoreditch
+            bushwick. Activated charcoal jean shorts chillwave four dollar toast
+            meggings health goth cornhole.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
